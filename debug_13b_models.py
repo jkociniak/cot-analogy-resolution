00c241271fabeab3f932fe1babfafbe35aea9ed6
janko <jan.kociniak@gmail.com>
2023-05-30 15:24:20 +0200

    path fix

--- debug_13b_models.py
+++ debug_13b_models.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     # script needs to be run from root of repo
 
     # LLAMA 13B
-    cfg_ckpt = 'pretrained/llama-13b'
+    cfg_ckpt = '/home/lcur1680/uva-atcs-project/cot-analogy-resolution/pretrained/llama-13b'
     model_params = {
         "cfg_ckpt": cfg_ckpt,
         "weights_ckpt": cfg_ckpt,
